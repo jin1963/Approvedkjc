ac53c6cdb2347abedf2e547f9e765799c09f9845
jin1963 <kaojino89@gmail.com>
2025-07-28 12:54:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
 let web3;
 let kjcToken;
 let user;
-
-async function connectWallet() {
-  if (window.ethereum || window.bitkeep?.ethereum) {
-    web3 = new Web3(window.ethereum || window.bitkeep.ethereum);
-    const accounts = await web3.eth.requestAccounts();
-    user = accounts[0];
-    document.getElementById("status").innerText = "✅ Connected: " + user;
-    kjcToken = new web3.eth.Contract(erc20ABI, kjcAddress);
-  } else {
-    alert("Please install MetaMask or Bitget Wallet");
-  }
-}
-
 async function approveKJC() {
-  if (!kjcToken || !user) {
+  if (!window.web3 || !window.ethereum) {
     alert("Please connect wallet first.");
     return;
   }
+
+  web3 = new Web3(window.ethereum || window.bitkeep?.ethereum);
+  const accounts = await web3.eth.getAccounts();
+  user = accounts[0];
+  kjcToken = new web3.eth.Contract(erc20ABI, kjcAddress);
 
   try {
     await kjcToken.methods.approve(spenderAddress, approveAmount).send({ from: user });
